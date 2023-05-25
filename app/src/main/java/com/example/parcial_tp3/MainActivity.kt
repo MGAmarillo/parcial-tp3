d3ac74779142c0762775eaa15dcca5ac3fb08853
MGAmarillo <mateo.amarillo50@gmail.com>
2023-05-25 19:39:56 -0300

    Creation Splash Screen + Menu + Home

--- app/src/main/java/com/example/parcial_tp3/MainActivity.kt
+++ app/src/main/java/com/example/parcial_tp3/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.parcial_tp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        val hamburguerMenu = findViewById<ImageView>(R.id.imageMenu)
+            hamburguerMenu.setOnClickListener(){
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        navigationView.itemIconTintList = null
     }
 }
--- app/src/main/java/com/example/parcial_tp3/SplashActivity.kt
+++ app/src/main/java/com/example/parcial_tp3/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.parcial_tp3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
